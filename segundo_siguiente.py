37327cbff9a12a8bd7a3b7bed8c7e4cca2b820cc
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-12 08:15:06 -0600

    Programa 6 listo

--- segundo_siguiente.py
+++ segundo_siguiente.py
@@ -1,15 +1,31 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Larisa Carolina Alvarez Gonzalez 763374
+Ximena Castro Lopez 743955
+Angelica Ibarra Diaz 761017
+Marco Fabricio Azpeitia Castellanos 765168
+
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+horas = int(input("Ingrese las horas: "))
+minutos = int(input("Ingrese los minutos: "))
+segundos = int(input("Ingrese los segundos: "))
+
 
 # Proceso
-salida = entrada
+segundos = int(segundos) + 1
+if segundos > 59:
+    minutos += 1
+    segundos = 0
+    if minutos > 59:
+        minutos = 0
+        horas += 1
+        if horas > 24:
+            horas = 0
+
+hora_final = " Hora: " + str(horas) + "\n Minutos: " + str(minutos) + "\n Segundos: " + str(segundos)
 
 # Salidas
-print(salida)
+print("\n")
+print("Hora final: " )
+print(hora_final)
